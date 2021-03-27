2c593bea426d138ebbe0c8b45cc15ad842a914b5
sreevishnu211 <shreevishnu456@gmail.com>
2021-03-27 14:14:30 +0530

    58. Length of Last Word

---
+++ 58LengthOfLastWords.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/length-of-last-word/
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len( s.strip().split( ' ' )[-1] )
