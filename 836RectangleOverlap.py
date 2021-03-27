9aee66f27086da43e33c0bde8066aceae611c288
sreevishnu211 <shreevishnu456@gmail.com>
2021-03-27 23:48:10 +0530

    836. Rectangle Overlap

---
+++ 836RectangleOverlap.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isRectangleOverlap(self, rec1, rec2):
+        """
+        :type rec1: List[int]
+        :type rec2: List[int]
+        :rtype: bool
+        """
+        #making sure its not a line
+        if ( rec1[0] == rec1[2] or rec1[1] == rec1[3] or
+             rec2[0] == rec2[2] or rec2[1] == rec2[3] ):
+            return False
+        
+        #making sure if the sides overlap
+        if rec1[0] >= rec2[2]: return False
+        if rec1[2] <= rec2[0]: return False
+        if rec1[1] >= rec2[3]: return False
+        if rec1[3] <= rec2[1]: return False
+        
+        return True
