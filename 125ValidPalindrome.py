5947848caedcf7cf1de043915bf5afa4e20cc8bf
sreevishnu211 <shreevishnu456@gmail.com>
2021-03-29 01:38:04 +0530

    125. Valid Palindrome

---
+++ 125ValidPalindrome.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/valid-palindrome/
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join( [ i.lower() for i in s if i.isalnum() ] )
+        return s == s[ : : -1 ]
