e747af845eb8774add4537851dd03489456956f9
sreevishnu211 <shreevishnu456@gmail.com>
2021-03-31 23:57:13 +0530

    28. Implement strStr()

---
+++ 28ImplementstrStr.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/implement-strstr/
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
